c7b59e9500410c4eee7f20163c0f093891fe95a8
semu <mail@semu.mp>
2012-09-09 19:02:34 +0200

    Changed bootstrap handling

--- create.sh
+++ create.sh
@@ -14,19 +14,14 @@ echo 'Creating files'
 express -f --sessions --css less .
 
 echo 'Downloading latest bootstrap'
-git clone git://github.com/twitter/bootstrap.git
-
-echo 'Moving files'
-cp bootstrap/docs/assets/css/bootstrap-responsive.css public/stylesheets/bs-r.css
-cp bootstrap/docs/assets/css/bootstrap.css public/stylesheets/bs.css
-rm -rf bootstrap
+curl https://raw.github.com/twitter/bootstrap/master/docs/assets/css/bootstrap-responsive.css -o public/stylesheets/bs-r.css
+curl https://raw.github.com/twitter/bootstrap/master/docs/assets/css/bootstrap.css -o public/stylesheets/bs.css
 
 echo 'Copying example files'
 cp -r sample.layout.jade views/layout.jade
 rm sample.layout.jade
 cp -r sample.main.js public/javascripts/main.js
 rm sample.main.js
-
 
 echo 'Downloading RequireJS'
 curl http://requirejs.org/docs/release/jquery-require/jquery1.8.0-requirejs2.0.6/jquery-require-sample.zip -O
